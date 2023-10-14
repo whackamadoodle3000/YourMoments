2ab519187f820e2435bc82aaf6c72f1d6d0118c3
Pranav Tadepalli <orca.pranav@gmail.com>
2023-10-13 23:25:36 -0400

    moment

---
+++ mom.py
@@ -0,0 +1,36 @@
+from moviepy.video.io.VideoFileClip import VideoFileClip
+import os
+import glob
+
+def find_newest_file(folder_path):
+    list_of_files = glob.glob(os.path.join(folder_path, '*.mov'))
+    latest_file = max(list_of_files, key=os.path.getctime)
+    return latest_file
+
+def split_into_10s(input_file, output_folder):
+    clip = VideoFileClip(input_file)
+
+    # Get the duration of the video in seconds
+    duration = clip.duration
+
+    # Calculate the number of 10-second clips
+    num_clips = int(duration // 10)
+
+    # Create MOM10s folder if it doesn't exist
+    output_folder_path = os.path.join(os.getcwd(), output_folder)
+    os.makedirs(output_folder_path, exist_ok=True)
+
+    # Split the video into 10-second clips
+    for i in range(num_clips):
+        start_time = i * 10
+        end_time = (i + 1) * 10
+        subclip = clip.subclip(start_time, end_time)
+        subclip.write_videofile(os.path.join(output_folder_path, f"clip_{i + 1}.mp4"), codec="libx264")
+
+    clip.reader.close()
+    clip.audio.reader.close_proc()
+
+if __name__ == "__main__":
+    input_file = "MOMents/your_video.mov"
+    output_folder = "MOM10s"
+    split_into_10s(input_file, output_folder)
