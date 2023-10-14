f21969f8e36d006ac107b08baf58abc93388bc14
snonk <selena.c.qiao@gmail.com>
2023-10-14 01:12:37 -0400

    face

---
+++ face.py
@@ -0,0 +1,36 @@
+from deepface import DeepFace
+import PIL
+
+DEFAULT_FILENAME = "tmp.jpg"
+
+def save_pil(image, filename = DEFAULT_FILENAME):
+    image.save(filename)
+
+'''
+Takes in a PIL Image and path to database of named reference face images. 
+Image names in the database should match names of the people.
+
+Returns name of face if it finds one. 
+'''
+def id_face(pil_img, db_path):
+    save_pil(pil_img)
+    dfs = DeepFace.find(img_path = DEFAULT_FILENAME, db_path = db_path)
+    return dfs[0].sort_values("VGG-Face_cosine")["identity"][0].split("/")[-1][:-4]
+
+'''
+Takes in a PIL Image.
+Returns top emotion of face if there is a face. If there is no face, returns None.
+'''
+def id_emotion(pil_img):
+    save_pil(pil_img)
+    try:
+        emotion = DeepFace.analyze(img_path = DEFAULT_FILENAME, actions = ['emotion'])
+        return max(emotion[0]['emotion'].keys(), key = lambda k: emotion[0]['emotion'][k])
+    except:
+        return None # No face
+
+if __name__ == "__main__":
+    # id_face()
+    im1 = PIL.Image.open("images/lana1.jpg")
+    save_pil(im1)  
+    print(id_face(im1, "images/"))
